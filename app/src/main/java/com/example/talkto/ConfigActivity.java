e85b903cbc239f0b280c8969cd48348355212a81
Arthur Pinheiro <arthurppinheiro@hotmail.com>
2024-05-15 13:24:44 -0300

    added themes implementatio

--- app/src/main/java/com/example/talkto/ConfigActivity.java
+++ app/src/main/java/com/example/talkto/ConfigActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,6 +25,12 @@ public class ConfigActivity extends AppCompatActivity {
 
         ImageButton returnButton = findViewById(R.id.returnButton);
         Button saveButton = findViewById(R.id.saveButton);
+        SwitchCompat darkModeSwitch = findViewById(R.id.darkModeSwitch);
+
+        boolean isDarkModeEnabled = (getResources().getConfiguration().uiMode & android.content.res.Configuration.UI_MODE_NIGHT_MASK)
+                == android.content.res.Configuration.UI_MODE_NIGHT_YES;
+
+        darkModeSwitch.setChecked(isDarkModeEnabled);
 
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +43,12 @@ public class ConfigActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (darkModeSwitch.isChecked()) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+
                 Intent returnChat = new Intent(ConfigActivity.this, ChatActivity.class);
                 startActivity(returnChat);
             }
